ef0505cd9e31184d1157d225495f177781c3e81c
Hezekiah Elisha <elishahezekiah903@gmail.com>
2025-07-22 20:52:15 +0300

    feat: add experience editing functionality and update routes

--- models/user.model.go
+++ models/user.model.go
@@ -6,12 +6,8 @@ import (
 
 type User struct {
 	gorm.Model
-	// ID        uint           `json:"id" gorm:"primaryKey"`
 	Username string `json:"username" gorm:"unique;not null"`
 	Email    string `json:"email" gorm:"unique;not null"`
 	Password string `json:"password" gorm:"not null"`
 	Role     string `json:"role" gorm:"default:'user'"`
-	// CreatedAt time.Time      `json:"created_at" gorm:"not null autoCreateTime"`
-	// UpdatedAt time.Time      `json:"updated_at" gorm:"not null autoUpdateTime"`
-	// DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index null"`
 }
